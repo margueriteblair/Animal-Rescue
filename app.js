b251c62874f6c03495432f470270f62e03442c59
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-01 14:40:46 -0400

    ternary and conditional to validate the type of mongoose uri

--- app.js
+++ app.js
@@ -13,9 +13,12 @@ let app = express(); //your application is an instance of express
 app.use(express.static('public')); //allows static html files to be process
 app.use(express.json()) //allows json files to be processed
 
-mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true}, (error) => {
-    console.error(error.message || error);
-})
+
+if (typeof(process.env.MONGO_URL) === "string") {
+    mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true}, (error) => {
+        console.log(error ? `\nError Connecting to MongoDB: ${error.message || error} \n` : `Server Connected to DB`
+        )})        
+} else {console.error('Invalid connection to database, URI type isnt a string')}
 
 //imported
 const homeRouter = require('./routes/homeRouter');
