1639c22a8c53d939c4728bccd2a2c3f83a241248
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-01 08:46:54 -0400

    post request for request body created by animal model and form

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const AnimalModel = require('../models/animalModel')
 
 router.post('/', async (req, res) => {
     try {
-
+        await AnimalModel.create(req.body);
+        res.json({message: `${req.body.email} successfully submitted adoption request!`})
     } catch (error) {
-        await 
+        console.error(error.message || error);
+        res.status(500).json({message: error.message});
     }
 })
 
